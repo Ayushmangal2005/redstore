4abc83351c5093b2a8027a0a5e839d6ffa9c8161
Ayushmangal2005 <ayushmangal152@gmail.com>
2024-11-19 23:45:54 +0530

    search

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py craetesuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
